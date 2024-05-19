eb57385700a138b56d487bcadca0e9b203f06d97
yui <nhankiet0501@gmail.com>
2024-05-19 11:39:29 +0700

    Now we can build the image and run the app with just only docker with deploy.sh. No need to install Go on your machine.

--- backend-go/deploy.sh
+++ backend-go/deploy.sh
@@ -1,8 +1,3 @@
-echo "Downloading packages..."
-go mod download
-echo "Compiling..."
-GOARCH=arm64 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app-linux
-
 echo "Building docker image..."
 docker build -t cinema .
 docker tag cinema:latest cinema:staging
