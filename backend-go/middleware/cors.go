bdeeacfada1376f591cf3b1c942a0beb2bbd06e7
yui <nhankiet0501@gmail.com>
2024-06-15 14:26:14 +0700

    update cors middleware

--- backend-go/middleware/cors.go
+++ backend-go/middleware/cors.go
@@ -2,13 +2,17 @@ package middleware
 
 import (
 	"cinema/component/appctx"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-func AllowCORS(_ appctx.AppContext) gin.HandlerFunc {
-	cfg := cors.DefaultConfig()
-	cfg.AllowAllOrigins = true
-	cfg.AllowCredentials = true
-	return cors.New(cfg)
+func CORSMiddleware(_ appctx.AppContext) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Content-Type", "application/json")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Next()
+	}
 }
