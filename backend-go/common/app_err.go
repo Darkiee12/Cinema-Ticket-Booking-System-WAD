cae57a12ec23515d8784c32a87b76fea870701dc
yui <nhankiet0501@gmail.com>
2024-06-17 10:29:27 +0700

    monitoring?

--- backend-go/common/app_err.go
+++ backend-go/common/app_err.go
@@ -11,7 +11,7 @@ type AppError struct {
 	StatusCode int    `json:"status_code"`
 	RootErr    error  `json:"-"`
 	Message    string `json:"message"`
-	Log        string `json:"log"`
+	Log        string `json:"-"`
 	Key        string `json:"error_key"`
 }
 
