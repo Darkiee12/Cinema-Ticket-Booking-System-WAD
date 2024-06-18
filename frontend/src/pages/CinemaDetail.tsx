f8da9546ee235d85f80c0149262db1f293285d8d
Darkness <kizluxury0506@gmail.com>
2024-06-19 02:55:20 +0700

    add banner column

--- frontend/src/pages/CinemaDetail.tsx
+++ frontend/src/pages/CinemaDetail.tsx
@@ -24,20 +24,20 @@ const CinemaDetail = () => {
 
     console.log("cinema",cinema);
 
-    return (
+    return cinema && (
         <div className="max-w-[1040px] h-screen mx-auto bg-[#FDF7DC]">
             <p className="w-full text-center text-black text-[25px] font-semibold font-Montserrat pt-2">Cinema detail</p>
-            <div className="flex flex-col items-center h-full">
-                {/* <div className="w-full">
-                    <img src={cinema?.poster} alt={cinema?.name} className="w-full min-h-[16rem]" />
-                </div> */}
-                <div className="mt-4">
-                    <p className="text-xl font-bold">{cinema?.name}</p>
-                    <p className="text-lg">{cinema?.address}</p>
-                    <p className="text-lg">{cinema?.phone_number}</p>
-                    <p className="text-lg">{cinema?.email}</p>
+            <div className="flex items-center">
+                <div className="w-1/3 flex justify-center items-center">
+                    <img src={cinema.banner} alt={cinema.name} className="w-full aspect-auto" />
+                </div>
+                <div className="w-2/3 p-2">
+                    <p className="text-xl font-bold">{cinema.name}</p>
+                    <p className="text-lg">{cinema.address}</p>
+                    <p className="text-lg">{cinema.phone_number}</p>
+                    <p className="text-lg">{cinema.email}</p>
                     {/* <p className="text-lg">{cinema!.website}</p> */}
-                    <p className="text-lg">Auditoriums: {cinema?.capacity}</p>
+                    <p className="text-lg">Auditoriums: {cinema.capacity}</p>
                 </div>
             </div>
         </div>
